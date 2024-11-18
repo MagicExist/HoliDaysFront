012246867a78d244bdc6f6de714c3fb942f03d5e
target-id <codeberrio@gmail.com>
2024-11-18 07:05:27 -0500

    añadiendo variables de entorno

--- src/app/Services/holi-days-http-handler.service.ts
+++ src/app/Services/holi-days-http-handler.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HoliDayClass } from '../models/holi-day-class';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,15 @@ export class HoliDaysHttpHandlerService {
   constructor(private Client:HttpClient) { }
 
   public ValidateHoliDay(date:string[]):Observable<any>{
-    const url = "https://localhost:7235/Api/festivos/verificar/"+date[0] + "/" + date[1] + "/" + date[2]
+    const url = `${environment.apiBase}/festivos/verificar/${date[0]}/${date[1]}/${date[2]}`
     console.log(url)
     return this.Client.get<any>(url)
   }
 
   public GetHoliDaysByYear(year:string):Observable<any>{
-    const url = "https://localhost:7235/Api/festivos/obtener/"+year
+    const url = `${environment.apiBase}/festivos/obtener/${year}`
     return this.Client.get<any>(url)
   }
 
+  //variable de ambiente y
 }
